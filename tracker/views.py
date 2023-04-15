2da824e3f2e2b0caffcc09f13f70d78c57d7e324
Akua Apeagyei <apeagyei.akua@gmai.com>
2023-04-15 03:06:29 +0100

    Created database and models

--- tracker/views.py
+++ tracker/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
+from .models import Opportunity
 
-opportunities = [
-    {
-        'author': 't-aapeagyei',
-        'title': 'NHS Logic Apps',
-        'content': 'Join the DevOps Squad hackathon to create a logic app!',
-        'date_posted': 'April 03/04/2023'
-    },
-    {
-        'author': 't-aapeagyei',
-        'title': 'Power Apps Mip',
-        'content': 'Present a 3 day Power App Mip to TFL',
-        'date_posted': 'April 04/04/2023'
-    }
-]
 
 def home(request):
     return render(request, 'tracker/home.html', {'title': 'Home'})
 
 def customer_project(request):
     context = {
-        'opportunities': opportunities
+        'opportunities': Opportunity.objects.all()
     }
     return render(request, 'tracker/customer_project.html', context)
 
