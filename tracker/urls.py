46a6072522a85e3669f5ea10af07d2d3a8a68e28
Akua Apeagyei <apeagyei.akua@gmai.com>
2023-04-18 10:22:45 +0100

    creating, deleting and update logic

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from .views import OpportunityListView,OpportunityDetailView, OpportunityCreateView, OpportunityUpdateView, OpportunityDeleteView
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='tracker-home'),
+    path('', OpportunityListView.as_view(), name= 'tracker-home'),
+    path('opportunity/<int:pk>/', OpportunityDetailView.as_view(), name='opportunity-detail'),
+    path('opportunity/new', OpportunityCreateView.as_view(), name='opportunity-create'),
+    path('opportunity/<int:pk>/update/', OpportunityUpdateView.as_view(), name='opportunity-update'),
+    path('opportunity/<int:pk>/delete/', OpportunityDeleteView.as_view(), name='opportunity-delete'),
     path('customer_project/', views.customer_project, name='tracker-customer_project'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Opportunity
 
 
@@ -11,3 +13,49 @@ def customer_project(request):
     }
     return render(request, 'tracker/customer_project.html', context)
 
+class OpportunityListView(ListView):
+    model = Opportunity
+    template_name = 'tracker/home.html'
+    context_object_name = 'opportunities'
+    ordering = ['-date_posted']
+
+
+class OpportunityDetailView(DetailView):
+    model = Opportunity
+   
+
+class OpportunityCreateView(LoginRequiredMixin,CreateView):
+    model = Opportunity
+    fields = ['title', 'content']
+    success_url = '/customer_project'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class OpportunityUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Opportunity
+    fields = ['title', 'content']
+    success_url = '/customer_project'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        opportunity = self.get_object()
+        if self.request.user == opportunity.author:
+            return True
+        return False
+    
+
+class OpportunityDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Opportunity
+    success_url = '/customer_project'
+   
+    def test_func(self):
+        opportunity = self.get_object()
+        if self.request.user == opportunity.author:
+            return True
+        return False
