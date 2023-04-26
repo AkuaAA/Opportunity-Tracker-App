4f9f337461e35dacb4a3580d4a675d2a58c35fd4
Akua Apeagyei <apeagyei.akua@gmai.com>
2023-04-26 14:47:21 +0100

    update settings

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -127,9 +127,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
